da5e78e4e6f27aa4d2676b4e28dc83e423beb64c
joshwyatt <joshuawyatt123@gmail.com>
2015-05-15 17:28:33 -0700

    Rename file

---
+++ sublime/shortcuts.js
@@ -0,0 +1,93 @@
+Chrome / Sublime / and Bash / and most apps with tabs:
+
+shift + command + n : create a new window
+command + n : create new tab
+command + w : close tab or window if only one tab
+shift + command + w : close widow with multiple open tabs
+command + q : quit
+command + shift + ] : move right one tab
+command + shift + [ : move left one tab
+command + NUMBER : move to tab number NUMBER
+fn + up : page up
+fn + down : page down
+
+Vimium (install it first): https://vimium.github.io/
+j : scroll down
+k : scroll up
+f : show links
+
+Chrome:
+pin tabs
+command + r : reload page
+command + option + j : open the javascript console
+command + ] : move forward through history
+command + [ : move backward through history
+command + shift + t : reopen last closed tab (up to 10)
+
+Sublime:
+command + ctrl + f : toggle fullscreen
+command + ctrl + shift + f : toggle distraction free
+
+option + arrows : move one word at a time
+command + arrows : move to beginning and end of line
+shift + above commands : select all the text
+delete + the above : delete all the text
+fn + delete : delete forward
+command + k, command k : delete to end of line
+command + k, command delete : delete to beginning of line
+fn + up / down : page up / page down
+fn + right / left : beginning and end of file
+
+command + z : undo
+command + shift + z : redo
+command + / : toggle comment for line
+command + ] : shift indenting right
+command + [ : shift indenting left
+command + l : select the whole line
+command + c (with selection) : copy selection
+command + x (with selection) : cut selection
+command + v : paste most recent cut or copy
+command + shift + v : paste with proper indentation
+
+var outerFunction = function(){
+
+
+
+};
+
+var innerFunction = function(){
+
+};
+
+
+command + x : cut the whole line
+command + c : copy the whole line
+command + enter : create new line below
+command + shift + enter : create new line above
+command + shift + d : copy current line below
+command + control + arrows : move entire line up and down
+command + k, command + u : make selection upper case
+command + k, command + l : make selection lower case
+command + ' : select all within quotes
+var quote = "Wouldn't it be nice to replace this quote?";
+command + shift + ' : select all within quotes, and quotes
+var quote = "Wouldn't it be nice to replace this quote with a number?";
+control + m : move to beginning/end of parens and braces
+if('only there were a way to toggle around the parens'){
+  var it = 'would be rad';
+}
+
+command + d : select word(s)
+command + k while selecting words : don't select word
+command + ctrl + g : select all of words
+option + mouseDrag : column select
+command + mouseClick : custom multiple cursor
+
+command + p : goto anything (use with :line_number, @symbol, #term)
+command + r : search all function names in current file
+command + shift + p : set syntax (and many many other things)
+command + f : find
+command + shift + f : massive find
+command + k, command + b : toggle file bar
+command + option + NUMBER : split screen
+control + shift + NUMBER : move current file to pane
